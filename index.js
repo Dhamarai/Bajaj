bccc76833149e95f7e48f40c1ce872281d74ce91
Dhamaraiselvi S <120491039+Dhamarai@users.noreply.github.com>
2024-08-25 12:08:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ app
 
     res.json({
       is_success: true,
-      user_id: "sanhita17",
-      email: "sanhita.kundu2020@vitstudent.ac.in",
-      roll_number: "20BEC0215",
+      user_id: "dhamaraiselvi",
+      email: "dhamaraiselvi.s2021@vitstudent.ac.in",
+      roll_number: "21BBS0271",
       numbers: numbers,
       alphabets: alphabets,
       highest_alphabet: highest_alphabet ? [highest_alphabet] : [],
